a65b2b9095ef890187f611073b789ca2b2074e14
Laurent Le Brun <laurentlb@gmail.com>
2024-10-31 15:37:59 +0100

    update comment

--- main3.cc
+++ main3.cc
@@ -1,6 +1,7 @@
 #include <iostream>
-//#include "third_party/foo/sub.hh"
-#include "sub.hh"
+#include "third_party/foo/sub.hh"
+//#include "sub.hh"
+//#include "pkg/foo/sub.hh"
 
 int main() {
     std::cout << "hello " << hello << "\n";
